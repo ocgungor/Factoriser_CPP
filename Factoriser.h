1c6864f95efec744f5e8f647a285bb41a2c99611
Oguzhan Gungor <cansingungor@gmail.com>
2013-04-14 09:43:03 +1000

    Documentation added and readme updated

--- Factoriser.h
+++ Factoriser.h
@@ -41,8 +41,9 @@ using namespace std;
 class Factoriser
 {
 public:
+    //Factorise a given integer into an array
 	vector<int> factorise(int n);	
-
+    //print the factors in the array using printf()
 	void printFactors(vector <int> &factors, int n);
 };
 
